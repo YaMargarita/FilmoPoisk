a767e29532849ad8225096dfdd8224d20c3eb220
YaMargarita <iakovenko.margarita@gmail.com>
2021-03-26 11:51:16 +0300

    Add components

---
+++ src/components/Collection.vue
@@ -0,0 +1,31 @@
+<template>
+  <Film :title="title"
+        :showFilm="filmList"></Film>
+</template>
+
+<script>
+import Film from "@/components/Film";
+export default {
+  name: "Collection",
+  data(){
+    return{
+      filmList: [],
+      title: '',
+      id: ''
+    }
+  },
+  components: {
+    Film
+  },
+  async mounted() {
+    this.id = this.$router.currentRoute.params.id;
+    let films = await this.$store.dispatch('SHOW_FILMS', this.id)
+    this.filmList = films.list
+    this.title = films.title
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Search">
+    <Film :title="title" :showFilm="searchFilm"></Film>
+  </div>
+</template>
+
+<script>
+import Film from "@/components/Film";
+export default {
+  name: "Search",
+  components: {
+    Film
+  },
+  data(){
+    return {
+      title: '',
+      searchFilm: [],
+      id: '',
+    }
+  },
+  async mounted(){
+    this.id = this.$router.currentRoute.params.id;
+
+    this.searchFilm = await this.$store.dispatch('SEARCH_FILM', this.id)
+    this.title = `По Вашему запросу найдено ${this.searchFilm.length} совпадений`
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
